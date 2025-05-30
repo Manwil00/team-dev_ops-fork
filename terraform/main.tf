1140e1ef0fed468be3a930b3afde689bc1e3bbc3
Ghaith Allah Chebil <32078979+ghaithallahchebil@users.noreply.github.com>
2025-05-30 04:52:17 +0200

    Created main.tf

---
+++ terraform/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-0c02fb55956c7d316"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "MyEC2Instance"
+  }
+}
+
+output "instance_ip" {
+  value = aws_instance.example.public_ip
+}
