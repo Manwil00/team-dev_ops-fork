421adeddb0d68f95f0172dd5b29dc7870f8dfc97
ge74tif <manuel.wilhelm@tum.de>
2025-07-14 20:15:43 +0200

    tf test

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -18,6 +18,20 @@ data "aws_instance" "existing_instance" {
   instance_id = var.instance_id
 }
 
+# Configure S3 backend for state management
+terraform {
+  backend "s3" {
+    bucket         = "niche-state-bucket" # Replace with your S3 bucket name
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+  }
+}
+
+# Reference an existing EC2 instance using the variable
+data "aws_instance" "existing_instance" {
+  instance_id = var.instance_id
+}
+
 # Retrieve the subnet details to get the VPC ID
 data "aws_subnet" "instance_subnet" {
   id = data.aws_instance.existing_instance.subnet_id
@@ -91,4 +105,13 @@ resource "aws_security_group" "app_sg_created" {
 # Create a local variable to hold the correct security group ID.
 locals {
   app_sg_id = length(data.aws_security_groups.existing_sg_lookup.ids) == 0 ? aws_security_group.app_sg_created[0].id : data.aws_security_groups.existing_sg_lookup.ids[0]
+}
+
+
+# Attach the security group only if it's not already attached
+resource "aws_network_interface_sg_attachment" "sg_attachment" {
+  count = contains(data.aws_instance.existing_instance.network_interfaces[0].security_groups[*], local.app_sg_id) ? 0 : 1
+
+  security_group_id    = local.app_sg_id
+  network_interface_id = data.aws_instance.existing_instance.network_interfaces[0].network_interface_id
 }
